975188169fa459ef9e34580d4afcf0c120331c0c
Hokage-om <director.maltiiti@gmail.com>
2025-03-10 17:58:32 +0530

    Add files via upload

---
+++ Functions/index.js
@@ -0,0 +1,13 @@
+function nice(name) {
+    console.log("Hey " + name + " You are intelligent")
+}
+
+nice("Abhinav")
+nice("Om")
+
+function sum(a,b) {
+    return a + b
+}
+
+result = sum(9,8)
+console.log("The sum is: ", result)
